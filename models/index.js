e6e88fb4cf071f88fe00c90938e37c6a18440b62
ChangJoo Park <pcjpcj2@gmail.com>
2018-06-07 19:40:05 +0900

    Use chaining for create object

--- models/index.js
+++ models/index.js
@@ -1,24 +1,29 @@
 const path = require('path')
 const readdirpSync = require('fs-readdirp').readdirpSync
+
 const capitalize = require('../utils/capitalize')
+
 const mongoose = require('../db')
 const Schema = mongoose.Schema
 
-const files = readdirpSync(__dirname, (filePath, stats) => {
-  if (stats.isDirectory()
-    || filePath.includes('index.js')
-    || !filePath.includes('.js')
-  ) {
-    return false
-  }
-  return {
-    path: filePath,
-    name: path.basename(filePath).replace('.js', '')
-  }
-})
-.map(({ name, path }) => mongoose.model(capitalize(name), new Schema(require(path))))
-
 const models = {}
-files.forEach(model => models[model.modelName] = model)
+
+const files = readdirpSync(__dirname, (filePath, stats) => {
+    if (stats.isDirectory() ||
+      filePath.includes('index.js') ||
+      !filePath.includes('.js')
+    ) {
+      return false
+    }
+    return {
+      path: filePath,
+      name: path.basename(filePath).replace('.js', '')
+    }
+  })
+  .map(({
+    name,
+    path
+  }) => mongoose.model(capitalize(name), new Schema(require(path))))
+  .forEach(model => models[model.modelName] = model)
 
 module.exports = models
