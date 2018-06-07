62f9a20202166feadf396268d595d0b74ff89b67
ChangJoo Park <pcjpcj2@gmail.com>
2018-06-07 14:47:22 +0900

    Add run server codes

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Hello World')
+const app = require('./server')
+const config = require('./config')
+
+app.listen(config.PORT, () => {
+  console.log(`Server starting ${config.PORT}`)
+})
